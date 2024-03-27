965d72f4730877f39c3fcc5a1ce9c87b8ef585ee
Joshua Yang <jyangftw@gmail.com>
2024-03-26 19:04:17 -0700

    heroku

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,28 @@ const bodyParser = require("body-parser");
 const userRouter = require("./routes/userRouter");
 const roomRouter = require("./routes/roomRouter");
 const questionRouter = require("./routes/questionRouter");
+const dotenv = require("dotenv");
 
 const app = express();
 
+dotenv.config();
+
 app.use(express.json()); // <==== parse request body as JSON
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public"))); //set static folder to public
+// app.use(express.static(path.join(__dirname, "public"))); //set static folder to public
 
 app.use("/api/user", userRouter);
 app.use("/api/question", questionRouter);
 app.use("/api/room", roomRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html")));
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 const http = require("http");
-const app = require("./app");
-const server = http.createServer(app);
 const socket = require("./socket/socketInit");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-//configure environment variables
-dotenv.config({ path: "./config.env" });
+// //configure environment variables
+dotenv.config();
 
 //connect to Atlas Server Database
 let DB_Connection_String = process.env.DATABASE_STRING.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
@@ -14,6 +12,9 @@ DB_Connection_String = DB_Connection_String.replace("<DATABASE_NAME>", "db");
 mongoose.connect(DB_Connection_String).then(() => {
   console.log("MongoDB connection succesful!");
 });
+
+const app = require("./app");
+const server = http.createServer(app);
 
 //initialize http server and mount socket.io instance on same port
 const port = process.env.PORT || 3000;
